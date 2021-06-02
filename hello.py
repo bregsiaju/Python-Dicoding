b906d944ec738923c32f1b4a939391e6329ec22f
bregsiaju <bregsia@gmail.com>
2021-06-03 02:21:13 +0700

    add reviewer class

---
+++ hello.py
@@ -0,0 +1,13 @@
+# define a function
+def world():
+    print("Hello World!")
+
+nama = "Bregsi"
+
+class Reviewer:
+    def __init__(self, nama, kelas):
+        self.nama = nama
+        self.kelas = kelas
+    
+    def Reviewer(self):
+        print("Reviewer " + self.nama + " bertanggung jawab di kelas " + self.kelas)
