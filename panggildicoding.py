c12fddc6e580b7ea2161f724b45b2334906e251a
bregsiaju <bregsia@gmail.com>
2021-06-04 19:34:00 +0700

    memodifikasi argumen

--- panggildicoding.py
+++ panggildicoding.py
@@ -1,8 +1,7 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-o', '--output', action='store_true', help='tampilkan output')
+parser.add_argument('-n', '--nama', required=True, help="Masukkan nama Anda")
 args = parser.parse_args()
 
-if args.output:
-    print("Halo, ini merupakan sebuah output dari panggildicoding.py")
+print("Terima kasih telah menggunakan panggildicoding.py, "+args.nama)
