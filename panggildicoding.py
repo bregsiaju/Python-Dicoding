105f56ece6741b90b65770a2f45e7e587124b28f
bregsiaju <bregsia@gmail.com>
2021-06-04 21:57:25 +0700

    menambahkan argumen dan if utk panggilan sblm nama

--- panggildicoding.py
+++ panggildicoding.py
@@ -2,6 +2,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', '--nama', required=True, help="Masukkan nama Anda")
+parser.add_argument('-t', "--tahunlahir", required=True, help="Masukkan tahun tahir Anda (YYYY)")
 args = parser.parse_args()
 
-print("Terima kasih telah menggunakan panggildicoding.py, "+args.nama)
+if args.tahunlahir <= '1990':
+    print("Terima kasih telah menggunakan panggildicoding.py pada tahun 2020, bapak "+args.nama)
+else:
+    print("Terima kasih telah menggunakan panggildicoding.py pada tahun 2020, kakak "+args.nama)
